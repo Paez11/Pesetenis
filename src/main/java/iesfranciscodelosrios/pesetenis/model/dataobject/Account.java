cf9ebc142a21fdef3d2ff7635342fff13ab54c58
JoseCP82 <josecamarap@gmail.com>
2023-01-12 18:48:55 +0100

    customer and account models implemented

--- src/main/java/iesfranciscodelosrios/pesetenis/model/dataobject/Account.java
+++ src/main/java/iesfranciscodelosrios/pesetenis/model/dataobject/Account.java
@@ -1,4 +1,68 @@
 package iesfranciscodelosrios.pesetenis.model.dataobject;
 
 public class Account {
+
+    /**
+     * Attributes
+     */
+    private double balance;
+    private boolean transaction;
+
+    /**
+     * Constructor with full params
+     * @param balance Total account balance
+     */
+    public Account(double balance) {
+        this.balance = balance;
+        this.transaction = false;
+    }
+
+    /**
+     * Default constructor
+     */
+    public Account() {
+        this(200000);
+        this.transaction=false;
+    }
+
+    /**
+     * Getters and Setters
+     */
+    public double getBalance() { return balance; }
+    public void setBalance(double balance) { this.balance = balance; }
+    public boolean isTransaction() { return transaction; }
+    public void setTransaction(boolean transaction) { this.transaction = transaction; }
+
+    /**
+     * Withdraws an amount from the account
+     * @param name Customer name
+     * @param amount Amount to withdraw
+     */
+    public synchronized void drawBalance(String name, double amount) {
+        double initialBalance=this.balance;
+        if(this.balance>0 && this.balance-amount>=0) {
+            this.balance -= amount;
+            //Mostar saldo inicial en cuenta y disponible tras operacion
+
+        } else {
+            if(this.balance==0){
+                //No se puede retirar cantidad, saldo 0
+
+            } else {
+                //saldo insuficiente
+
+            }
+        }
+    }
+
+    /**
+     * Increase account balance
+     * @param @param name Customer name
+     * @param amount Amount to increase
+     */
+    public synchronized void enterBalance(String name, double amount) {
+        double initialBalance=this.balance;
+        this.balance += amount;
+        //Mostar saldo inicial en cuenta y disponible tras operacion
+    }
 }
---
+++ src/main/java/iesfranciscodelosrios/pesetenis/model/dataobject/Customer.java
@@ -0,0 +1,63 @@
+package iesfranciscodelosrios.pesetenis.model.dataobject;
+
+public class Customer extends Thread {
+
+    /**
+     * Attributes
+     */
+    private String customerName;
+    private double amount;
+    private String transactionType;
+    private Account account;
+
+    /**
+     * Constructor with full params
+     * @param name Customer name
+     * @param amount Amount to carry out the transaction
+     * @param transactionType Transaction to perform, enter or extract
+     * @param account Customer account
+     */
+    public Customer(String name, double amount, String transactionType, Account account) {
+        this.customerName = name;
+        this.amount = amount;
+        this.transactionType = transactionType;
+        this.account = account;
+    }
+
+    /**
+     * Getters and Setters
+     */
+    public String getCustomerName() { return customerName; }
+    public void setCustomerName(String customerName) { this.customerName = customerName; }
+    public double getAmount() { return amount; }
+    public void setAmount(double amount) { this.amount = amount; }
+    public String getTransactionType() { return transactionType; }
+    public void setTransactionType(String transactionType) { this.transactionType = transactionType; }
+    public Account getAccount() { return account; }
+    public void setAccount(Account account) { this.account = account; }
+
+    /**
+     * Run method overridden
+     * Start a type of transaction
+     */
+    @Override
+    public void run() {
+        if(account.isTransaction()){
+            synchronized (account){
+                try {
+                    account.wait();
+                    account.setTransaction(false);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } else {
+            if(transactionType.equals("enter")){
+                account.enterBalance(customerName, amount);
+            } else if(transactionType.equals("extract")) {
+                account.drawBalance(customerName,amount);
+            }
+            account.setTransaction(true);
+        }
+    }
+}
